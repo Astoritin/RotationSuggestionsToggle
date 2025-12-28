60565a5f3c8b9b82056340d0c574dfc41d330ebe
Astoritin <Astoritin@outlook.com>
2025-12-28 16:58:49 +0800

    Remove unnecessary functions

--- module/action.sh
+++ module/action.sh
@@ -2,27 +2,24 @@
 MODDIR=${0%/*}
 
 MOD_INTRO="Toggle the showing of rotation suggestions button."
-SEPARATE_LINE="***************************************"
 
-echo "$SEPARATE_LINE"
-echo "- Rotation Suggestions Toggle"
-echo "- By Astoritin"
-echo "$SEPARATE_LINE"
-echo "- $MOD_INTRO"
-echo "$SEPARATE_LINE"
+echo " "
+echo " Rotation Suggestions Toggle"
+echo " By Astoritin"
+echo " "
+echo " $MOD_INTRO"
+echo " "
 result_rs="$(settings get secure show_rotation_suggestions)"
 if [ $result_rs = 0 ]; then
-    echo "- Current state: OFF"
+    echo " Current state: OFF"
     settings put secure show_rotation_suggestions 1
-    echo "- Turn on suggestions button"
-    echo "- Now button will be shown as detecting rotation"
+    echo " Turn on suggestions button"
+    echo " Now button will be shown as detecting rotation"
 elif [ $result_rs = 1 ]; then
-    echo "- Current state: ON"
+    echo " Current state: ON"
     settings put secure show_rotation_suggestions 0
-    echo "- Turn off suggestions button"
-    echo "- Now button will NOT be shown as detecting rotation"
+    echo " Turn off suggestions button"
+    echo " Now button will NOT be shown as detecting rotation"
 fi
-echo "$SEPARATE_LINE"
-echo "- Done!"
 sleep 1
 exit 0
--- module/customize.sh
+++ module/customize.sh
@@ -5,93 +5,6 @@ MOD_PROP="${TMPDIR}/module.prop"
 MOD_NAME="$(grep_prop name "$MOD_PROP")"
 MOD_VER="$(grep_prop version "$MOD_PROP") ($(grep_prop versionCode "$MOD_PROP"))"
 
-is_magisk() {
-
-    if ! command -v magisk >/dev/null 2>&1; then
-        return 1
-    fi
-
-    MAGISK_V_VER_NAME="$(magisk -v)"
-    MAGISK_V_VER_CODE="$(magisk -V)"
-    case "$MAGISK_V_VER_NAME" in
-        *"-alpha"*) MAGISK_BRANCH_NAME="Alpha" ;;
-        *"-lite"*)  MAGISK_BRANCH_NAME="Magisk Lite" ;;
-        *"-kitsune"*) MAGISK_BRANCH_NAME="Kitsune Mask" ;;
-        *"-delta"*) MAGISK_BRANCH_NAME="Magisk Delta" ;;
-        *) MAGISK_BRANCH_NAME="Magisk" ;;
-    esac
-    DETECT_MAGISK="true"
-    return 0
-
-}
-
-is_kernelsu() {
-    if [ -n "$KSU" ]; then
-        DETECT_KSU="true"
-        ROOT_SOL="KernelSU"
-        return 0
-    fi
-    return 1
-}
-
-is_apatch() {
-    if [ -n "$APATCH" ]; then
-        DETECT_APATCH="true"
-        ROOT_SOL="APatch"
-        return 0
-    fi
-    return 1
-}
-
-install_env_check() {
-
-    MAGISK_BRANCH_NAME="Official"
-    ROOT_SOL="Magisk"
-    ROOT_SOL_COUNT=0
-
-    is_kernelsu && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
-    is_apatch && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
-    is_magisk && ROOT_SOL_COUNT=$((ROOT_SOL_COUNT + 1))
-
-    if [ "$DETECT_KSU" = "true" ]; then
-        ROOT_SOL="KernelSU"
-        ROOT_SOL_DETAIL="KernelSU (kernel:$KSU_KERNEL_VER_CODE, ksud:$KSU_VER_CODE)"
-    elif [ "$DETECT_APATCH" = "true" ]; then
-        ROOT_SOL="APatch"
-        ROOT_SOL_DETAIL="APatch ($APATCH_VER_CODE)"
-    elif [ "$DETECT_MAGISK" = "true" ]; then
-        ROOT_SOL="Magisk"
-        ROOT_SOL_DETAIL="$MAGISK_BRANCH_NAME (${MAGISK_VER_CODE:-$MAGISK_V_VER_CODE})"
-    fi
-
-    if [ "$ROOT_SOL_COUNT" -gt 1 ]; then
-        ROOT_SOL="Multiple"
-        ROOT_SOL_DETAIL="Multiple"
-    elif [ "$ROOT_SOL_COUNT" -lt 1 ]; then
-        ROOT_SOL="Unknown"
-        ROOT_SOL_DETAIL="Unknown"
-    fi
-
-}
-
-show_system_info() {
-
-    ui_print "- Device: $(getprop ro.product.brand) $(getprop ro.product.model) ($(getprop ro.product.device))"
-    ui_print "- OS: Android $(getprop ro.build.version.release) (API $(getprop ro.build.version.sdk)), $(getprop ro.product.cpu.abi | cut -d '-' -f1)"
-    ui_print "- Kernel: $(uname -r)"
-
-}
-
-print_line() {
-
-    length=${1:-39}
-    symbol=${2:-*}
-
-    line=$(printf "%-${length}s" | tr ' ' "$symbol")
-    ui_print "$line"
-
-}
-
 extract() {
     file=$1
     dir=$2
@@ -131,23 +44,15 @@ fi
 
 ui_print "- Setting up $MOD_NAME"
 ui_print "- Version: $MOD_VER"
-install_env_check
-show_system_info
-ui_print "- Installing from $ROOT_SOL app"
-ui_print "- Root: $ROOT_SOL_DETAIL"
 extract 'customize.sh' "$TMPDIR"
 extract 'action.sh'
 extract 'module.prop'
 extract 'uninstall.sh'
-ui_print "- Setting permission"
+ui_print "- Setting permissions"
 set_perm_recursive "$MODPATH" 0 0 0755 0644
 ui_print "- Closing rotation suggestions button"
-settings put secure show_rotation_suggestions 0 && ui_print "- Done"
-print_line
-ui_print ""
+settings put secure show_rotation_suggestions 0
 ui_print "- $MOD_NAME"
 ui_print "- will take effect immediately"
-ui_print "- Reboot is NOT required if you don't"
+ui_print "- Reboot is not required if you don't"
 ui_print "- need to enable suggestions button later"
-ui_print ""
-print_line
